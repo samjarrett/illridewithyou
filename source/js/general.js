b8a7a1c140d17aa7a861b482bf4df30b8856e7dd
Sam Jarrett <samstah@gmail.com>
2014-12-17 08:00:30 +1100

    Fix up various bits, implement real copy

--- source/js/general.js
+++ source/js/general.js
@@ -1,6 +1,12 @@
 $(function() {
-	$("a[href^='http']").attr('target', '_blank').on('click', function() {
-		ga('send', 'event', 'Outbound Link', 'click', $(this).attr('href'));
+	$("a[href^='http']").on('click', function(e) {
+		e.preventDefault();
+		var url = $(this).attr('href');
+		ga('send', 'event', 'Outbound Link', 'click', url, {
+			'hitCallback': function () {
+				document.location = url;
+			}
+		});
 	});
 	$("a[href^='mailto:']").on('click', function() {
 		ga('send', 'event', 'Mailto Link', 'click', $(this).attr('href'));
@@ -20,4 +26,40 @@ $(function() {
 			$container.css({'margin-top': '1.5em'});
 		}
 	}).trigger('resize');
+
+	window.fbAsyncInit = function () {
+		FB.Event.subscribe('edge.create', function (targetUrl) {
+			ga('send', 'social', 'facebook', 'like', targetUrl);
+		});
+		FB.Event.subscribe('edge.remove', function (targetUrl) {
+			ga('send', 'social', 'facebook', 'unlike', targetUrl);
+		});
+		FB.Event.subscribe('message.send', function (targetUrl) {
+			ga('send', 'social', 'facebook', 'send', targetUrl);
+		});
+	};
+
+	function extractParamFromUri(uri, paramName) {
+		if (!uri) {
+			return;
+		}
+		var regex = new RegExp('[\\?&#]' + paramName + '=([^&#]*)');
+		var params = regex.exec(uri);
+		if (params != null) {
+			return unescape(params[1]);
+		}
+		return;
+	}
+	
+	twttr.ready(function (twttr) {
+		twttr.events.bind('tweet', function (intent_event) {
+			if (intent_event) {
+				var opt_pagePath;
+				if (intent_event.target && intent_event.target.nodeName == 'IFRAME') {
+					opt_target = extractParamFromUri(intent_event.target.src, 'url');
+				}
+				ga('send', 'social', 'twitter', 'tweet', opt_pagePath);
+			}
+		});
+	});
 });
